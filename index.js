28cffa631be62f71d1654603109fdea747337575
Alberto Valenzuela <avalen28@gmail.com>
2022-12-10 12:41:23 +0100

    lab WIP- 3 iterations remaining

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items.sort(function (a, b) {
+      return a - b;
+    });
+  }
+  get(pos) {
+    if (this.length <= pos) {
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  add(item) {}
-
-  get(pos) {}
-
-  max() {}
+  max() {
+    let maxVal = 0;
+    if (this.length < 1) {
+      throw new Error("EmptySortedList");
+    }
+    for (let i = 0; i < this.length; i++) {
+      if (this.items[i] > maxVal) {
+        maxVal = this.items[i];
+      }
+    }
+    return maxVal;
+  }
 
   min() {}
 
